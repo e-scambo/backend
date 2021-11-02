2b5545ad1feec6b293d3f74c28b064779ff5f3e8
tdutra <thiagodutra@gmail.com>
2021-11-02 18:54:11 -0300

    feat(announcement): added dto and renamed controller

--- src/presentation/dto/announcement.dto.ts
+++ src/presentation/dto/announcement.dto.ts
@@ -1,6 +1,8 @@
 import {
+  Equals,
   IsDefined,
   IsIn,
+  IsMongoId,
   IsNotEmpty,
   IsString,
   MaxLength,
@@ -11,6 +13,7 @@ import { AnnouncementTypes } from '../enum/announcement.enum';
 import { IsAlphaWithSpace } from '../validator/is.alpha.with.space';
 import { IsValidLocalization } from '../validator/is.valid.localization';
 import { IsValidText } from '../validator/is.valid.text';
+import { UserParamByIdDTO } from './user.dto';
 
 export class CreateAnnouncementDTO {
   @IsDefined()
@@ -55,6 +58,67 @@ export class CreateAnnouncementDTO {
   usage_time: string;
 
   images: ImageDTO[];
+  owner: string;
+}
+
+export class FindAnnouncementDto extends UserParamByIdDTO{
+
+  @IsMongoId()
+  announcement_id: string;
+
+}
+
+export class UpdateAnnouncementDto {
+
+  @ValidateIf((dto) => dto.title !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsValidText()
+  @MinLength(1)
+  @MaxLength(100)
+  title: string;
+
+  @ValidateIf((dto) => dto.description !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsValidText()
+  @MinLength(1)
+  @MaxLength(400)
+  description: string;
+
+  @ValidateIf((dto) => dto.type !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(Object.values(AnnouncementTypes))
+  type: string;
+
+  @ValidateIf((dto) => dto.category !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsAlphaWithSpace()
+  category: string;
+
+  @ValidateIf((dto) => dto.localization !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsValidLocalization()
+  localization: string;
+
+  @ValidateIf((dto) => dto.usage_time !== undefined)
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @IsAlphaWithSpace()
+  usage_time: string;
+
+  @Equals(undefined, {message: `This parameter can't be update through this path, please use image's controller resource.`})
+  images: ImageDTO[];
+
   owner: string;
 }
 
