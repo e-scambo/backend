86e619b00e5f66bfa10a08dd1534afffe1b2eec2
Lucas Rocha <lucascosmorocha@gmail.com>
2021-11-18 15:57:56 -0300

    feat(favorite): add module
    add interceptor

--- src/config/module/app.module.ts
+++ src/config/module/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from '../../presentation/controller/app.controller';
 import { AnnouncementModule } from './announcement.module';
+import { FavoriteModule } from './favorite.module';
 import { ImageModule } from './image.module';
 import { UserModule } from './user.module';
 
@@ -14,6 +15,7 @@ import { UserModule } from './user.module';
       retryAttempts: 3,
     }),
     AnnouncementModule,
+    FavoriteModule,
     ImageModule,
     UserModule,
   ],
