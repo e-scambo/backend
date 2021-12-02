2926a42d262803ee5028d5aefa6be46a31bf677a
tdutra <thiagodutra@gmail.com>
2021-12-02 09:25:19 -0300

    feat(add-announcent): corrected delete image feature

--- src/business/util/image.util.ts
+++ src/business/util/image.util.ts
@@ -2,7 +2,7 @@ import { randomBytes } from "crypto";
 export class ImageUtil {
   
   static generateImageName(ownerId: string, mimetype: string): string {
-    const time = Date.now();
+    const time = new Date().getTime();
     const randomHash = randomBytes(8).toString('hex');
     const format = mimetype.split('/')[1];
 
@@ -10,7 +10,7 @@ export class ImageUtil {
   }
 
   static isImageOwner(ownerId: string, imgName: string): Boolean {
-    const owner = imgName.split('_')[2]
-    return owner.substring(0, owner.indexOf('.') - 1) === ownerId
+    const owner = imgName.split('_')[3]
+    return owner.substring(0, owner.indexOf('.')) === ownerId
   }
 }
