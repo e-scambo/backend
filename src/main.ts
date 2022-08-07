a0fde58b04e58da9b1a53fccf9b497a7066aaf47
Lucas carvalho <61708642+magao02@users.noreply.github.com>
2022-08-07 15:12:08 -0300

    Update main.ts
    alterando cors

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const whitelist = [CLIENT];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  });
+  app.enableCors();
 
   setMiddlewares(app);
   await app.listen(PORT);
